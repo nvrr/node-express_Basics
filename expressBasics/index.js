a6e67f12b971c85005b5d4856dfbd44289d03299
nvrr <sureshvarma809@gmail.com>
2022-09-03 00:07:58 -0700

    HTTP put req

--- expressBasics/index.js
+++ expressBasics/index.js
@@ -24,24 +24,50 @@ app.get('/api/courses/:id', (req,res)=>{
 })
 
 app.get('/api/courses', (req,res)=>{
-    res.send([1,2,3])
+    res.send(courses)
 })
 
-app.post('/app/courses', (req,res) => {
+app.post('/api/courses', (req, res) => {
+   
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
 
-    if(!req.body.name || req.body.name.length < 3){
-        res.status(400).send("Name is required and should be minimum 3 characters")
+    const validity = schema.validate(req.body);
+    if(validity.error){
+        res.status(400).send(validity.error.details[0].message);
         return;
     }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {    
+    var course = courses.find(c => {return c.id === parseInt(req.params.id)});
+    if (!course)  {
+        res.status(404).send('The course with the given ID was not found')
+        return;
+    };
+
+    
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    const validity = schema.validate(req.body);
+    if(validity.error){
+        res.status(400).send(validity.error.details[0].message);
+        return;
     }
 
-    courses.push(course)
-    res.send(course)
-})
-
+    course.name = req.body.name;
+    res.send(course);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`listening on ${port} port....`))
